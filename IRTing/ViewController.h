12c9d4511613d92b0419cfdd362cb15edec8dd02
zhouning@incamel.com <zhouning@incamel.com>
2016-08-24 16:45:49 +0800

    项目创建1

---
+++ IRTing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IRTing
+//
+//  Created by zhongdai on 16/8/24.
+//  Copyright © 2016年 irena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios,'8.0'
+
+target 'IRTing' do
+	pod 'Reveal-iOS-SDK'
+	pod "MJExtension"
+	pod 'AFNetworking', '~> 3.0'
+	pod 'MBProgressHUD', '~> 0.9.2'
+    pod 'Masonry'
+    pod 'SDWebImage'
+    pod 'MJRefresh'
+end
